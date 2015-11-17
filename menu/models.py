6f6c6ba8b73dd0cf1062bc611f169cb56bf2a7d3
Jeff Reiher <jeffreiher@gmail.com>
2015-11-17 08:45:05 -0500

    add models.py with Place class to get started

---
+++ menu/models.py
@@ -0,0 +1,16 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+#### class code #######
+class Place(Base):
+	__tablename__ = 'restaurant' # names table
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+
+########## insert at end of file ########
+engine = create_engine('sqlite:///menu.db')
+Base.metadata.create_all(engine)
